14c3a617935b6ac85f78096f5946d1108a104ad6
Gallon <864101119@qq.com>
2023-07-21 14:32:23 +0800

    第一次更新

--- 数据分析.py
+++ 数据分析.py
@@ -84,45 +84,45 @@ path = 'C:/Users/86410/Desktop/试验数据/试验报告'
 des_path = 'C:/Users/86410/Desktop/试验数据/最后数据'
 
 
-for i in range(1, 8):
-    print(path+str(i))
-    for file_name in os.listdir(path+str(i)):
-        document = Document(path+str(i) + '/' + file_name)
-        tables = document.tables
-        first_table = tables[0]
-        place = ''
-        time = ''
-        cell_row = 0
-        for row in first_table.rows:
-            cell_column = 0
-            for cell in row.cells:
-                if cell.text == '工作地点':
-                    place = row.cells[cell_column+1].text
-                    print(place)
-                elif cell.text == '工作开始时间':
-                    time = row.cells[cell_column+1].text.split('-')[0]
-                    print(time)
-                cell_column += 1
-            cell_row += 1
-
-        for table in tables[1:]:
-            item_list = []
-            tan_list = []
-            change_dr = []
-            if table.cell(0, 1).text == 'tanδ(%)' and len(table.cell(1, 1).text)>0:
-                # doc = Document()
-                # doc.add_table(table[count])
-                # doc.save(des_path + '/' + file_name)
-                for row in table.rows[1:-1]:
-                    item_list.append(row.cells[0].text)
-                    tan_list.append(row.cells[1].text)
-                    change_dr.append(row.cells[4].text)
-
-                data = pd.DataFrame({'变电站': place, '所属相别': item_list, 'tg(%)': tan_list, '电容变化': change_dr, '试验日期': time})
-                # data = data.drop(0, axis=1)
-                data.to_csv(des_path + '/' + file_name.split('.')[0]+'.csv', encoding='gbk', index=False)
-                break
-print('保存完毕')
+# for i in range(1, 8):
+#     print(path+str(i))
+#     for file_name in os.listdir(path+str(i)):
+#         document = Document(path+str(i) + '/' + file_name)
+#         tables = document.tables
+#         first_table = tables[0]
+#         place = ''
+#         time = ''
+#         cell_row = 0
+#         for row in first_table.rows:
+#             cell_column = 0
+#             for cell in row.cells:
+#                 if cell.text == '工作地点':
+#                     place = row.cells[cell_column+1].text.strip().split(',')[0]
+#                     print(place)
+#                 elif cell.text == '工作开始时间':
+#                     time = row.cells[cell_column+1].text.split('-')[0]
+#                     print(time)
+#                 cell_column += 1
+#             cell_row += 1
+#
+#         for table in tables[1:]:
+#             item_list = []
+#             tan_list = []
+#             change_dr = []
+#             if table.cell(0, 1).text == 'tanδ(%)' and len(table.cell(1, 1).text)>0:
+#                 # doc = Document()
+#                 # doc.add_table(table[count])
+#                 # doc.save(des_path + '/' + file_name)
+#                 for row in table.rows[1:-1]:
+#                     item_list.append(row.cells[0].text)
+#                     tan_list.append(row.cells[1].text)
+#                     change_dr.append(row.cells[4].text)
+#
+#                 data = pd.DataFrame({'变电站': place, '所属相别': item_list, 'tg(%)': tan_list, '电容变化': change_dr, '试验日期': time})
+#                 # data = data.drop(0, axis=1)
+#                 data.to_csv(des_path + '/' + file_name.split('.')[0]+'.csv', encoding='gbk', index=False)
+#                 break
+# print('保存完毕')
 res_file_list = []
 res_file_list = os.listdir(des_path)
 res_dic = {'变电站': '', '所属相别': '', 'tg(%)': '', '电容变化': '', '试验日期': '', 'tg(%)2': '', '电容变化2': '', '试验日期2': '', 'tg(%)3': '', '电容变化3': '', '试验日期3': ''}
@@ -130,7 +130,7 @@ res_list = []
 finish_list = []
 for item in res_file_list:
     # print(des_path+'/'+item)
-    data = pd.read_csv(des_path + '/' + item, encoding='gbk')
+    data = pd.read_csv(des_path + '/' + item, encoding='gbk') #逐个读取csv文件
     # res_dic.get('试验日期').append(i)
     if item.split('.')[0].split('(')[0].strip() not in finish_list:
         for i in range(len(data)):
@@ -142,16 +142,24 @@ for item in res_file_list:
             res_list.append(res_dic.copy())
     else:
         for index, i in enumerate(res_list):
-            if i.get('变电站') == data.loc[0]['变电站']:
-                for j in range(len(data)):
+            try:
+                if index == 608:
+                    print('a')
+                if i.get('变电站') == data.loc[0]['变电站'] and i.get('所属相别') == '高压套管-A':
                     if res_list[index]['tg(%)2'] == '':
-                        res_list[index+j]['tg(%)2'] = data.loc[j]['tg(%)']
-                        res_list[index+j]['电容变化2'] = data.loc[j]['电容变化']
-                        res_list[index+j]['试验日期2'] = data.loc[j]['试验日期']
-                    elif res_list[index]['tg(%)2'] == '':
-                        res_list[index + j]['tg(%)3'] = data.loc[j]['tg(%)']
-                        res_list[index + j]['电容变化3'] = data.loc[j]['电容变化']
-                        res_list[index + j]['试验日期3'] = data.loc[j]['试验日期']
+                        for j in range(len(data)):
+                            res_list[index+j]['tg(%)2'] = data.loc[j]['tg(%)']
+                            res_list[index+j]['电容变化2'] = data.loc[j]['电容变化']
+                            res_list[index+j]['试验日期2'] = data.loc[j]['试验日期']
+
+                    elif res_list[index]['tg(%)3'] == '':
+                        for j in range(len(data)):
+                            res_list[index + j]['tg(%)3'] = data.loc[j]['tg(%)']
+                            res_list[index + j]['电容变化3'] = data.loc[j]['电容变化']
+                            res_list[index + j]['试验日期3'] = data.loc[j]['试验日期']
+                        # break
+            except Exception as e:
+                print(index)
     finish_list.append(item.split('.')[0].split('(')[0].strip())
     # res = pd.DataFrame({'变电站': place, '所属相别': item_list, 'tg(%)': tan_list, '电容变化': change_dr, '试验日期': time})
 pd.DataFrame(res_list).to_csv(des_path+'/整合数据.csv', index=False, encoding='gbk')
